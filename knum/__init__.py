2123f1fe697d5f269772e39d0ae9447903255b6d
ruby-kim <dev.rubykim@gmail.com>
2020-05-19 15:12:23 +0900

    Finish initial setting

---
+++ knum/__init__.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__name__ = 'knum'
+__description__ = 'change Korean tex number <---> Arabian number'
+__version__ = '0.0.0'
+__url__ = 'https://github.com/study-ai-data/knum'
+__download_url__ = 'https://github.com/study-ai-data/knum'
+__install_requires__ = [
+]
+__license__ = 'MIT'
+
+from knum.chgList import *
+from knum.chgFormat import *
---
+++ knum/chgFormat.py
@@ -0,0 +1,10 @@
+from knum import chgList
+
+
+def kor2num(word):
+    decode_result = []
+    return
+
+
+def num2kor(word):
+    return
