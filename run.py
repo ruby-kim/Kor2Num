514094986b8148377c65eab7eb29dbd1d5c2698a
ruby-kim <dev.rubykim@gmail.com>
2020-05-19 15:13:40 +0900

    Add main running script

---
+++ run.py
@@ -0,0 +1,19 @@
+from knum.chgFormat import chgList
+
+
+def find_num_text(text):
+    return
+
+
+def kor2num(text):
+    return
+
+
+def num2kor(text):
+    return
+
+
+if __name__ == "__main__":
+    find_num_text()
+    kor2num()
+    num2kor()
