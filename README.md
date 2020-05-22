4b4eab5c50462fa27142e4faf9e24c1fdfa376e7
ruby-kim <dev.rubykim@gmail.com>
2020-05-22 11:14:32 +0900

    Success kor to num demo

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # kor2num
+Change korean text num to 
 한글로 구성된 숫자를 아라비안 숫자로 바꿔주는 코드
+### Installation
+* pypi: ```pip install knum```
+* source code:
+  ```bash
+  git clone https://github.com/study-ai-data/knum.git
+  cd knum
+  python setup.py install
+  ```
+### Getting Started
+* pypi
+  ```python
+  from knum.chgFormat import kor2num
+  
+  
+  text = "맛있는 포카칩이 구천팔백칠십육억오천사백삼십이만천백십일원!"
+  result = kor2num(text)
+  print(result) # 맛있는 포카칩이 987654321111원!
+  ```
+* source code:
+  ```python run.py```
--- knum/chgFormat.py
+++ knum/chgFormat.py
@@ -55,6 +55,113 @@ def judge_exist_num_text(text):
     return cardResult, ordResult
 
 
+def find_num_text_section(text, Num, Idx):
+    firIdx = Idx
+    Lists = list()
+    for idx in Idx:
+        for i in range(idx, -1, -1):
+            if text[i] == ' ':
+                break
+            else:
+                for cards in Num:
+                    if text[i] == cards[0]:
+                        firIdx = i
+        Lists.append((firIdx, idx))
+    return Lists
+
+
+def text_of_num_text_section(text, Lists):
+    numText = list()
+    for elem in Lists:
+        numText.append(text[elem[0]:elem[1]])
+    return numText
+
+
+def separate_each_num_text_to_list(text, Lists, unitKorNum):
+    wordList = list()
+    for cardList in Lists:
+        tmp = ""
+        for i in range(cardList[1] - 1, cardList[0] - 1, -1):
+            if text[i] not in unitKorNum:
+                wordList.append(text[i] + tmp)
+                tmp = ""
+            else:
+                if i != cardList[0] and text[i - 1] in unitKorNum:
+                    wordList.append(text[i])
+                    continue
+                tmp = text[i] + tmp
+                if i == cardList[0]:
+                    wordList.append(tmp)
+    wordList.reverse()
+    return wordList
+
+
+def calc_element(wordList):
+    result = 0
+    tmp = 0
+    for i in range(len(wordList)):
+        if i is not 0 and wordList[i] > wordList[i - 1]:
+            result += tmp * wordList[i]
+            tmp = 0
+        else:
+            tmp += wordList[i]
+    result += tmp
+    return result
+
+
+def cardFunction(text):
+    cardIdx, ordIdx = get_guessed_num_idx(text)
+    changeLists = chgList()
+    cardNum = changeLists.cardNum
+    ordLists = list()
+
+    """ cardinal num """
+    # 1. find num text section
+    cardLists = find_num_text_section(text, cardNum, cardIdx)
+    cardListsText = text_of_num_text_section(text, cardLists)
+
+    # 2. separate kor and num to list
+    korCardNum, realCardNum = list(), list()
+    for card in cardNum:
+        korCardNum.append(card[0])
+        realCardNum.append(card[1])
+
+    baseKorCardNum = korCardNum[:9]  # 일, 이, 삼, 사, 오, 육, 칠, 팔, 구
+    unitKorCardNum = korCardNum[9:]  # 십, 백, 천, 만, 억, 조, 경, 해
+    baseRealCardNum = realCardNum[:9]  # 일, 이, 삼, 사, 오, 육, 칠, 팔, 구
+    unitRealCardNum = realCardNum[9:]  # 십, 백, 천, 만, 억, 조, 경, 해
+
+    # 3. separate each num text to list
+    wordList = separate_each_num_text_to_list(text, cardLists, unitKorCardNum)
+
+    # 4. separate two length texts to one-one in specific condition
+    for i in range(len(wordList)):
+        if len(wordList[i]) == 2:
+            if wordList[i][1] != '십' and wordList[i][1] != '백' and wordList[i][1] != '천':
+                wordList.insert(i + 1, wordList[i][1])
+                wordList[i] = wordList[i][0]
+
+    # 5. change each num text to real num
+    for i in range(len(wordList)):
+        if len(wordList[i]) == 2:
+            tmp1 = baseRealCardNum[baseKorCardNum.index(wordList[i][0])]
+            tmp2 = unitRealCardNum[unitKorCardNum.index(wordList[i][1])]
+            wordList[i] = tmp1 * tmp2
+        else:
+            wordList[i] = realCardNum[korCardNum.index(wordList[i][0])]
+
+    # 6. multiply & plus all of elements
+    result = calc_element(wordList)
+
+    # 6. change entire num text(word) to real num
+    for elem in cardListsText:
+        text = text.replace(elem, str(result))
+
+    #     #여기서부터.. 이제 숫자로 진짜 변환
+
+    return text
+
+
 def kor2num(text):
     """
     Args:
@@ -62,55 +169,9 @@ def kor2num(text):
     Returns:
         :param: result(list)
     """
-    cardIdx, ordIdx = get_guessed_num_idx(text)
-    changeLists = chgList()
-    cardNum = changeLists.cardNum
-    cardLists, ordLists = list(), list()
-
-    """ cardinal num """
-    # 1. find num text section
-    firIdx = cardIdx
-    for idx in cardIdx:
-        for i in range(idx, -1, -1):
-            if text[i] == ' ':
-                break
-            else:
-                for cards in cardNum:
-                    if text[i] == cards[0]:
-                        firIdx = i
-        cardLists.append((firIdx, idx))
-
-    # 2. change kor to num
-    korCardNum, realCardNum = list(), list()
-    for card in cardNum:
-        korCardNum.append(card[0])
-        realCardNum.append(card[1])
-
-    baseKorCardNum = korCardNum[:9]         # 일, 이, 삼, 사, 오, 육, 칠, 팔, 구
-    unitKorCardNum = korCardNum[9:]         # 십, 백, 천, 만, 억, 조, 경, 해
-    baseRealCardNum = realCardNum[:9]       # 일, 이, 삼, 사, 오, 육, 칠, 팔, 구
-    unitRealCardNum = realCardNum[9:]       # 십, 백, 천, 만, 억, 조, 경, 해
-
-    wordList = list()
-    for cardList in cardLists:
-        tmp = ""
-        for i in range(cardList[1]-1, cardList[0]-1, -1):
-            if text[i] not in unitKorCardNum:
-                wordList.append(text[i]+tmp)
-                tmp = ""
-            else:
-                if i != cardList[0] and text[i-1] in unitKorCardNum:
-                    wordList.append(text[i])
-                    continue
-                tmp = text[i]+tmp
-                if i == cardList[0]:
-                    wordList.append(tmp)
-    wordList.reverse()
-    
-    for word in wordList:
-        #여기서부터.. 이제 숫자로 진짜 변환
-    print(wordList)
-    return text
+    # ordResult = ordFunction(text)
+    cardResult = cardFunction(text)
+    return cardResult
 
 
 def num2kor(text):
