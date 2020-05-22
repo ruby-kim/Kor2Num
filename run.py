8f74564b46496cc2a34d7a61446aa5ecd66ed0c8
ruby-kim <dev.rubykim@gmail.com>
2020-05-22 16:20:14 +0900

    Write test input

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from korNum.chgFormat import judge_exist_num_text, kor2num, num2kor
 
-
+ 
 if __name__ == "__main__":
     text = "맛있는 포카칩이 구천팔백칠십육억오천사백삼십이만천백십일원에서 오백십만육십구원, 만오천구백원을 거쳐 이제는 만오천원으로!"
     # cardResult, ordResult = judge_exist_num_text(text)
